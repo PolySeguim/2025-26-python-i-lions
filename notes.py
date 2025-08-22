b47cc9d16063924513f925ecd565b1e57caf9477
Abby Miles <abby.miles0928@gmail.com>
2025-08-22 11:26:50 -0400

    notes.py

--- notes.py
+++ notes.py
@@ -1,1 +1,21 @@
-print("hello world")
+# def is the keyword to define a function
+# whatType is the name of the function
+# userinput is the parameter of the function
+def whatType(userinput):
+    #print is a Python built-in function that prints to the console
+    # type is a Python built-in function that finds the datatype
+    #
+    print (type(userinput)) 
+
+# The pound symbol is for one line comments
+# The program ignores all comments
+
+""" 
+multiple line comments
+"""
+#Call the function with different user inputs
+whatType(3)
+whatType(3.0)
+whatType(True)
+whatType("abbymiles")
+whatType('a')
