7638dfac56edbc309200ea0dca0f1b1a1655a25c
meb2608 <mebailey2608@gmail.com>
2025-08-22 15:15:31 +0000

    Main

---
+++ notes.py
@@ -0,0 +1,20 @@
+#def is the keyword to define a function 
+#whatType is the name of the function 
+#userInput is he parameter of the Function
+def whatType(userInput):
+    print(type(userInput))
+#print is a Python built-in function that prints to he console
+# $type is a Python built' in function that finds the datatype
+# unserInput is the variable that the user enters             
+
+
+# The pound symbol is for one line comments 
+# The program ignores all comments 
+"""
+multiple line comments
+"""
+whatType(3)
+whatType(3.0)
+whatType("polyana")
+whatType(True)
+whatType('p')
