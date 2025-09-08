73c92f04f2be3c0a8168151900aecad0845dc114
Abby Miles <abby.miles0928@gmail.com>
2025-09-08 13:12:54 -0400

    09-08 notes

---
+++ notes.py
@@ -0,0 +1,68 @@
+# def is the keyword to define a function
+# whatType is the name of the function
+# userInput is the parameter of the function
+def whatType(userinput):
+    #print is a Python built-in function that prints to the console
+    # type is a Python built-in function that finds the datatype
+    #userinput is the variable that the user enters
+    print (type(userinput)) 
+
+# The pound symbol is for one line comments
+# The program ignores all comments
+
+""" 
+multiple line comments
+"""
+#Call the function with different user inputs
+#is your dont call the function, nothing will run in the program
+"""
+TEST SUITE
+"""
+"""
+whatType(3)
+whatType(3.0)
+whatType(True)
+whatType("abbymiles")
+whatType('a')
+
+#create a variable named message
+message = 
+#This is a multiline message to my bestie."""
+
+print(message)
+
+age = 16
+print(type(age))
+age = float(age)
+print(type(age))
+
+#create a list of new emails
+
+#create a list of names
+studentNames
+emailAddress = "@ursulineacademy.org"
+
+for i in studentNames:
+#addition between two strings in cocatenation
+    email = student[i]+emailAddress
+    print(email)
+
+    
+#python allows different data types to be stored side by side in a list
+aList = ["poly", "ana", 48, 3.0, True]
+print(aList)
+"""
+
+#input function
+userName = input("What is your name? ")
+userAddress = input("What is your address? ")
+userCity = input("What is your city? ")
+userZip = input("What is your zip code? ")
+
+print(userName, userAddress, userCity, userZip)
+
+
+
+#Calculates the area of a circle
+diameter = float(input("Diameter: "))
+print("Area of a circle:" )
