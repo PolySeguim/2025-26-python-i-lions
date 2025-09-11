e04f148717585ed625333e20593c5c1bfd87124c
meb2608 <mebailey2608@gmail.com>
2025-09-11 14:30:00 +0000

    commitning changes to my code

--- #Exercises.py
+++ #Exercises.py
@@ -141,3 +141,83 @@ need to convert the user's input from degrees to radians before computing
 the distance with the formula discussed previously.  The math module 
 contains a function named RADIANS which converts from degrees to radians.
 """
+import math 
+t1 = float(input("What is the Latitude of point 1? "))
+g1 = float(input("What is the Longitude of point 1? "))
+t2 = float(input("What is the Latitude of point 2? "))
+g2 = float(input("What is the Longitude of point 2? "))
+#convert radians to degrees
+t1 = math.radians(t1)
+g1 = math.radians(g1)
+t2 = math.radians(t2)
+g2 = math.radians(g2)   
+distance = 6371.01 * math.acos((math.sin(t1) * math.sin(t2)) + (math.cos(t1) * math.cos(t2) * math.cos(g1-g2)))
+print("The distance between the two points is " + str(distance) + " km")   
+
+"""
+Exercise 13: Making Change
+Consider the software that runs on a self-checkout machine.  One task that
+it must be able to perform is to determine how much change to provide 
+when the shopper pays for a purchase with cash.
+
+Write a program that begins by reading a number of cents from the user
+as an integer.  Then your program should compute and display the 
+denominations of the coins that should be used to give that amount 
+of change  to the shopper.  The change should be given using as 
+few coins as possible.  Assume that the machine is loaded with pennies,
+nickels, dimes, quarters, loonies and toonies.
+
+*** A one dollar coin was introduced in Canada in 1987  It is referred
+to as a loonie because one side of the coin has a loon (type of bird) on it.
+The two dollar coin, referred to as a toonie, was introduced 9 years later.
+It's name is derived from the combination of the number two
+and the name of the loonie.
+"""
+cents = int(input("How many cents do you have? "))
+toonies = cents // 200
+cents %= 200
+loonies = cents // 100
+cents %= 100
+quarters = cents // 25
+cents %= 25
+dimes = cents // 10
+cents %= 10
+nickels = cents // 5
+cents %= 5
+pennies = cents // 1
+cents %= 1
+print("You have " + str(toonies) + "toonies" + "You have " + str(loonies) +"loonies" + "You have " + str(quarters) + "quarters" + "You have " + str(dimes) + "dimes" + "You have " + str(nickels) + "nickels" + "You have " + str(pennies) + "pennies")
+
+"""
+Exercise 14:  Height Units
+Many people think about their height in feet and inches, even in some
+countries that primarily use the metric system.
+Write a program that reads a number of feet from the user, followed 
+by a number of inches.  Once the values are read, your program should 
+compute and display the equivalent number of centimeters.
+
+*** HINT ***
+One foot is 12 inches.  One in ch is 2.54 centimeters.
+"""
+feet= int(input("What is your height in feet? "))
+inches = int(input( "What is your height in inches? "))
+totalinches = (feet * 12) + inches
+centimeters = totalinches * 2.54
+print("Your height in centimeters is " + str(centimeters) + " cm")
+"""
+Exercise 15:  Distance Units
+In this exercise, you will create a program that begins by reading
+a measurement in feet from the users.  Then your program should display
+the equivalent distance in inches, yards, and miles. 
+
+*** HINT ***
+1 inch = 0.02777778 yards = 0.00001578 miles
+36 inches = 1 yard = 0.00056818 miles
+63360 inches = 1760 yards = 1 mile
+"""
+feet = int(input("What is your distance in feet? "))
+inches = feet * 12
+yards = feet / 3
+miles = feet / 5280
+print("Your distance in inches is " + str(inches) + " inches" + "Your distance in yards is " + str(yards) + " yards" + "Your distance in miles is " + str(miles) + " miles")
+
